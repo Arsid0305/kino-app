28a2d060ffafeed25a533a7fc5d63d420cb773ed
Arsid <arsid0305@gmail.com>
2026-04-28 15:43:23 +0300

    Update design system

--- kino-design-system/sync-design.bat
+++ kino-design-system/sync-design.bat
@@ -1,9 +1,9 @@
 @echo off
 cd /d "C:\DATA\AI_OS\projects\Kino"
-echo Синхронизируем дизайн-систему...
+echo Syncing design system...
 git add kino-design-system/
 git commit -m "Update design system"
 git push
 echo.
-echo Готово! Дизайн-система обновлена в репо.
+echo Done! Design system updated in repo.
 pause
